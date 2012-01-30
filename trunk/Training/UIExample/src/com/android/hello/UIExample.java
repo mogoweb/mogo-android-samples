0fd95dc7de5d5d0190ac30d870bb13e4cdb616fd
mogoweb@gmail.com <mogoweb@gmail.com@b4f0a175-e320-ef6e-692d-97965fc151f1>
2012-01-30 01:08:48 +0000

    modify UIExample to add more element

--- trunk/Training/UIExample/src/com/android/hello/UIExample.java
+++ trunk/Training/UIExample/src/com/android/hello/UIExample.java
@@ -2,6 +2,10 @@ package com.android.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UIExample extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,18 @@ public class UIExample extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Add a handle to UI components
+        EditText nameEntry = (EditText)findViewById(R.id.name_entry);
+        nameEntry.setText("Enter your name here");
+        
+        Button okButton = (Button)findViewById(R.id.ok);
+        
+        okButton.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v) {
+        		setResult(RESULT_OK);
+        		finish();
+        	}
+        });
     }
 }
